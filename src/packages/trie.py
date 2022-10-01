2b2e45ce3a374fe14996661f40f978cb8eda99c2
Yiyi Wang <yiyi@michaels.com>
2022-09-30 23:43:41 -0500

    refactor code to remove unnecessary functionalities; add comments

--- src/packages/trie.py
+++ src/packages/trie.py
@@ -1,68 +1,103 @@
 from trienode import *
-
-class TrieDict:
-	def __init__(self, words):
+class Trie:
+	"""
+	Basic class of Trie. A DFA that can represents a regular expression
+	"""
+	def __init__(self, words, forward=True, backward=False):
+		"""
+		Parameters:
+		c2t: Character to nodes. A dictionary points to all roots of tries.
+		c2tr: Character to nodes(reverse). A dictionary points to all roots of reversely-stored tries.
+		null: If the trie set contains an empty string.
+		"""
 		self.c2t = {}
 		self.c2tr = {}
-		self.empty = set()
-		for i in range(len(words)):
-			self.save_word(words[i], i)
-			self.save_word_r(words[i], i)
+		self.null = False
+		for word in words:
+			if forward:
+				self.generate_forward_trie(word)
+			if backward:
+				self.generate_backward_trie(word)
 
-	def save_word(self, word, idx):
+	def generate_forward_trie(self, word):
 		size = len(word)
 		if len(word) == 0:
-			self.empty.add(idx)
+			# The word is an empty string.
+			self.null = True
 		else:
 			if word[0] in self.c2t:
+				# The root of the trie to be generated is already in the trie-set.
 				i = 1
 				cur = self.c2t[word[0]]
 				while (i < size) and (word[i] in cur.next):
+					"""
+					Loop until branching out of the trie or
+					the trie for this word alreayd exists.
+					"""
 					cur = cur.next[word[i]]
 					i += 1
 				if i >= size:
-					cur.idx = idx
+					# The trie for this word already exists. Set isword to True.
+					cur.isword = True
 				else:
+					# Branched out of the existing trie. Generate new branch.
 					while (i < size):
-						cur.next[word[i]] = Trie(cur.val + word[i])
+						cur.next[word[i]] = TrieNode(cur.val + word[i])
 						cur = cur.next[word[i]]
 						i += 1
-					cur.idx = idx
+					cur.isword = True
 			else:
-				cur = Trie(word[0])
+				"""
+				Root of the trie that starts with this initial character 
+				does exist in the trie-set. Generate a new trie. 
+				"""
+				cur = TrieNode(word[0])
 				self.c2t[word[0]] = cur
 				i = 1
 				while (i < size):
-					cur.next[word[i]] = Trie(cur.val + word[i])
+					cur.next[word[i]] = TrieNode(cur.val + word[i])
 					cur = cur.next[word[i]]
 					i += 1
-				cur.idx = idx
+				cur.isword = True
 
-	def save_word_r(self, word, idx):
+	def generate_backward_trie(self, word):
 		i = len(word) - 1
 		if i == -1:
+			self.null = True
+			# The word is an empty string.
 			pass
 		else:
 			if word[i] in self.c2tr:
+				# The root of the trie to be generated is already in the trie-set.
 				cur = self.c2tr[word[i]]
 				i -= 1
 				while (i >= 0) and (word[i] in cur.next):
+					"""
+					Loop until branching out of the trie or
+					the trie for this word alreayd exists.
+					"""
 					cur = cur.next[word[i]]
 					i -= 1
 				if i < 0:
-					cur.idx = idx
+					# The trie for this word already exists. Set isword to True.
+					cur.isword = True
 				else:
+					# Branched out of the existing trie. Generate new branch.
 					while (i >= 0):
-						cur.next[word[i]] = Trie(word[i] + cur.val)
+						cur.next[word[i]] = TrieNode(word[i] + cur.val)
 						cur = cur.next[word[i]]
 						i -= 1
-					cur.idx = idx
+					cur.isword = True
 			else:
-				cur = Trie(word[i])
+				"""
+				Root of the trie that starts with this initial character 
+				does exist in the trie-set. Generate a new trie. 
+				"""
+				cur = TrieNode(word[i])
 				self.c2tr[word[i]] = cur
 				i -= 1
 				while (i >= 0):
-					cur.next[word[i]] = Trie(word[i] + cur.val)
+					cur.next[word[i]] = TrieNode(word[i] + cur.val)
 					cur = cur.next[word[i]]
 					i -= 1
-				cur.idx = idx
+				cur.isword = True
--- src/packages/trienode.py
+++ src/packages/trienode.py
@@ -1,14 +1,25 @@
-
-class Trie:
-	def __init__(self, val):
+class TrieNode:
+	"""
+	Basic class of TrieNode.
+	"""
+	def __init__(self, val, isword=False):
+		"""
+		parameters:
+		val: A string represents the result from root of the trie to this node.
+		next: Character keys that points to next node in the trie.
+		isword: Indicates if val is a word in the set when trie is generated.
+		"""
 		self.val = val
 		self.next = {}
-		self.idx = None
+		self.isword = isword
 
 	def dfs(self):
-		ret = []
-		if self.idx != None:
-			ret += [(self.val, self.idx)]
+		"""
+		Start DFS from a node to print all possible words in the sub-trie of this node.
+		"""
+		ret = set()
+		if self.isword:
+			ret.add(self.val)
 		for key in self.next:
-			ret += self.next[key].dfs()
+			ret = ret.union(self.next[key].dfs())
 		return ret
