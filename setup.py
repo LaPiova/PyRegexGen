45b434781bc8d2103ecdacd10fccaa96598c75d3
Yiyi Wang <yiyi@michaels.com>
2022-10-19 14:58:57 -0500

    update pakcage installation

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-		required = f.read().splitlines()
 
 setup(
 	name='PyRegexGen',
 	version='0.1',
 	author="LaPiova",
 	author_email='schrodinghauer@gmail.com',
-	install_requires=required,
 	classifiers=[
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python :: 3.7',
@@ -18,6 +12,7 @@ setup(
 	packages=find_packages(),
 	description="A trie generator and search tool.",
 	python_requires=">=3.6",
+	include_package_data = True,
 	license="MIT license",
 	zip_safe=False,
 )
